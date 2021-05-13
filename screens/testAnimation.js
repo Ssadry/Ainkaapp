ddfa107b6643c34f4f405860511b679a55b554a6
Ssadry <43681842+AndryAlexis@users.noreply.github.com>
2021-05-13 14:12:21 +0200

    Pequeñas pruebas

--- screens/testAnimation.js
+++ screens/testAnimation.js
@@ -1,96 +1,21 @@
-import React, {useState} from 'react';
-import {Animated, Dimensions} from 'react-native';
+import React from 'react';
 import styled from 'styled-components/native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
-
-const WINDOW = 'window';
-const {width} = Dimensions.get(WINDOW);
-let {height} = Dimensions.get(WINDOW);
+import Icon from '../assets/services/Aprende_a_usar_Facebook.jpg'
 
 export default () => {
-    const bottomValue = useState(new Animated.Value(0))[0];
-
-    const move = (value) => {
-        Animated.timing(bottomValue, {
-            toValue: value,
-            duration: 500,
-            useNativeDriver: true
-        }).start();
-    };
-
     return (
-        <Container>
-            <Animated.View style={[{transform: [{translateY: height}]}]}>
-                <Pop/>
-            </Animated.View>
-            <Button onPress={() => {
-                height *= -1;
-                move(0);
-            }}>
-                <Text>
-                    Mover
-                </Text>
-            </Button>
+        <Container source={Icon} blurRadius={10}>
+            <Cube/>
         </Container>
     )
 }
 
-export const Pop = () => {
-    const [popIsVisible, setPopIsVisible] = useState(false);
-    
-
-    return (
-        <ContainerPop 
-            onPress={() => alert('cyan')}
-        >
-            <Close onPress={() => alert('rojo')}/>
-            <ContentPop>
-                Hola :)
-            </ContentPop>
-        </ContainerPop>
-    )
-}
-
-export const Container = styled.View`
-    justify-content: center;
-    align-items: center;
+export const Container = styled.ImageBackground`
     flex: 1;
 `;
 
-export const ContainerPop = styled.TouchableOpacity`
-    width: ${width + 'px'};
-    height: ${height + 'px'};
-    background-color: green;
-    position: relative;
-    bottom: ${-height + 'px'};
-    justify-content: center;
-    align-items: center;
-`;
-
-export const Button = styled.TouchableOpacity`
-    width: ${width + 'px'};
-    position: absolute;
-    bottom: 0px;
-    justify-content: flex-end;
-    border-width: 1px;
-    justify-content: center;
-    align-items: center;
-    background-color: white;
-`;
-
-export const Text = styled.Text`
-    margin: 20px;
-`;
-
-export const Close = styled.TouchableOpacity`
-    justify-content: center;
-    align-items: center;
+export const Cube = styled.View`
+    background-color: red;
     width: 100px;
     height: 100px;
-    background-color: red;
-`;
-
-export const ContentPop = styled.Text`
-
 `;
