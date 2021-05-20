98b36afb497393087a31899949922bc189de4652
Ssadry <43681842+AndryAlexis@users.noreply.github.com>
2021-05-20 21:46:19 +0200

    Añadido código de Carrusel

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {Dimensions, Platform, StatusBar} from 'react-native';
-import Navigation from './components/navigation/index';
-import Provider from './application/provider';
-import PostAdPopUp from './screens/postAd';
-import styled from 'styled-components/native';
+// import 'react-native-gesture-handler';
+// import React from 'react';
+// import {Dimensions, Platform, StatusBar} from 'react-native';
+// import Navigation from './components/navigation/index';
+// import Provider from './application/provider';
+// import PostAdPopUp from './screens/postAd';
+// import styled from 'styled-components/native';
 
-export default () => {
-    return (
-        <Container>
-            <Provider>
-                <Navigation/>
-                <PostAdPopUp/>
-            </Provider>
-        </Container>
-    );
+// export default () => {
+//     return (
+//         <Container>
+//             <Provider>
+//                 <Navigation/>
+//                 <PostAdPopUp/>
+//             </Provider>
+//         </Container>
+//     );
+// }
+
+// export const Container = styled.SafeAreaView`
+//     height: ${Dimensions.get('screen').height + 'px'};
+//     flex: 1;
+//     padding-top: ${Platform.OS === 'android' ? StatusBar.currentHeight + 'px' : 0};
+// `;
+
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import CarouselCards from './CarouselCards'
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <CarouselCards />
+    </SafeAreaView>
+  );
 }
 
-export const Container = styled.SafeAreaView`
-    height: ${Dimensions.get('screen').height + 'px'};
-    flex: 1;
-    padding-top: ${Platform.OS === 'android' ? StatusBar.currentHeight + 'px' : 0};
-`;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 50
+  },
+});
+
