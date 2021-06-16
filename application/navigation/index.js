5e2f7663c240430b8cc65b42b9d156a5344d723a
Ssadry <43681842+AndryAlexis@users.noreply.github.com>
2021-06-16 08:17:00 +0200

    ya no peta el cerrar sesión
    pero no está del todo terminado

--- application/navigation/index.js
+++ application/navigation/index.js
@@ -13,13 +13,16 @@ export const removeNavigationScreen = (navigation, routeName) => {
 
 export const resetNavigationHistory = (navigation) => {
     navigation.dispatch((state) => {
-        console.log(state);
-        const {history} = state.routes;
+        const {history} = state.routes[0].state;
+        console.log('Pre:')
+        console.log(history);
         const length = history.length;
         const routes = history[length - 1];
+        console.log('Pos:');
+        console.log(routes);
         return CommonActions.reset({
             ...state,
-            history: [routes]
+            history: []
         });
     });
 };
